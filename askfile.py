d17257efcde9f2d37d7c7c092c7044fc4378c89b
AdamMIAO <Adammiaohaoyu@163.com>
2020-02-23 16:13:04 +0800

    edit module

--- askfile.py
+++ askfile.py
@@ -5,3 +5,8 @@ def akf():
     x = tkinter.filedialog.askopenfilename(title="Select image", filetypes=fts)
     root.destroy()
     return x
+
+def aks():
+    import tkinter.filedialog, tkinter
+    root = tkinter.Tk()
+    fts = [('Image file', '.png')]
